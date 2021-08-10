1fae31ea6ee0399cc6858a9d5a7eb96956613c59
wy-hsieh <56617685+wy-hsieh@users.noreply.github.com>
2021-08-10 18:34:14 +0800

    test

---
+++ components/LargeCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <NuxtLink :to="`/products/${card.id}`" class="card">
+    <img :src="require(`~/assets/images/${card.image}`)" alt="" class="img">
+    <h3 class="header">
+      {{ card.title }}
+    </h3>
+    <div class="snippet">
+      {{ card.snippet }}
+    </div>
+  </NuxtLink>
+</template>
+
+<script>
+  export default {
+    props: ['card'],
+  }
+</script>
+
+<style scoped>
+.card {
+    width: 31.5%;
+    height: 25rem;
+    border: none;
+    overflow: hidden;
+    padding: 0;
+    cursor: pointer;
+}
+.image {
+    height: 65%;
+    border-radius: 0.5rem;
+}
+.header {
+    font-size: 1.15rem;
+    margin-top: 0.4rem;
+}
+.header:hover {
+  color: black;
+  text-decoration: none;
+}
+.snippet {
+    color: grey
+}
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,32 @@
+<template class="container">
+  <div>
+    <Nav />
+    <LargeCardDisplay 
+      v-for="cardInfo in largeCardInfo.slice(0, 1)"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo" 
+    />
+    <SmallCardDisplay 
+      v-for="cardInfo in smallCardInfo.slice(0, 1)"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo" 
+    />
+
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from '~/assets/data.js';
+  export default {
+    data() {
+      return {
+        largeCardInfo: largeCardSections,
+        smallCardInfo: smallCardSections,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
